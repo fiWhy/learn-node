ff4783b3922d54449854ccf895d388be97b13647
Denis Buryachkovskiy <zekatsi5@gmail.com>
2020-10-05 21:50:16 +0300

    add delete with id

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const http = require("http");
+const url = require("url");
+const queryString = require("querystring");
 const { read, write } = require("./utils");
 
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case "/add":
-      const messages = read("messages");
+  const parsedUrl = url.parse(request.url);
+  const { id } = queryString.parse(parsedUrl.query);
 
-      const newData = [
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  const messages = read("messages");
+  switch (parsedUrl.pathname) {
+    case "/add":
+      write("messages", [
         ...messages,
         {
           id: messages[messages.length - 1].id + 1,
           name: "No-name",
         },
-      ];
-
-      write("messages", newData);
+      ]);
       break;
+    case "/delete":
+      const newArray = [...messages];
+      if (id) {
+        const elementIndex = newArray.findIndex((el) => el.id === Number(id));
+
+        if (elementIndex !== -1) {
+          newArray.splice(elementIndex, 1);
+        }
+      } else {
+        newArray.splice(newArray.length - 1, 1);
+      }
+
+      write("messages", newArray);
   }
 
   response.end(JSON.stringify(read("messages")));
